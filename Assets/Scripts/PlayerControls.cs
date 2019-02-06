0ee2cd1c144fc045bf5204def6271e5459191670
Matthew Clark <matrclark@gmail.com>
2019-02-06 12:59:37 +0000

    Made player control smoother
    Player controls now use Time.deltaTime to make movement smoother

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -6,6 +6,10 @@ public class PlayerControls : MonoBehaviour
 {
 
     public bool isPlayerLeft = true;
+    bool moving = false;
+
+    Vector2 newPosition;
+    float t;
 
     // Start is called before the first frame update
     void Start()
@@ -20,28 +24,49 @@ public class PlayerControls : MonoBehaviour
         //     Debug.Log("1");
         // }
 
-        foreach(Touch touch in Input.touches) {
+        if (!moving)
+        {
+            foreach (Touch touch in Input.touches)
+            {
 
-            // Check the user has tapped the screen
-            if (touch.phase == TouchPhase.Began) {
-                // As the user has tapped the screen, they will move up one tile
-                Vector2 totalMovement = new Vector2 (0,1);
+                // Check the user has tapped the screen
+                if (touch.phase == TouchPhase.Began)
+                {
+                    t = 0;
+                    // As the user has tapped the screen, they will move up one tile
+                    Vector2 totalMovement = new Vector2(0, 0);
 
-                // Check if they move left or right
-                bool userTouchedLeft = touch.position.x < Screen.width/2;
+                    totalMovement.y ++;
 
-                if(!isPlayerLeft && userTouchedLeft) { // If the player is on the right and the user touched the left side
-                    totalMovement.x = -3.5f;
-                    isPlayerLeft = true; // Player is now on the left
+                    // Check if they move left or right
+                    bool userTouchedLeft = touch.position.x < Screen.width / 2;
+
+                    if (!isPlayerLeft && userTouchedLeft)
+                    { // If the player is on the right and the user touched the left side
+                        totalMovement.x = -3.5f;
+                        isPlayerLeft = true; // Player is now on the left
+                    }
+                    else if (isPlayerLeft && !userTouchedLeft)
+                    { // If the player is on the left and the user touched the right side
+                        totalMovement.x = 3.5f;
+                        isPlayerLeft = false; // Player is now on the right
+                    }
+
+                    // Set the new position
+                    newPosition = new Vector2(transform.position.x + totalMovement.x, transform.position.y + totalMovement.y);
+
+                    moving = true;
+                    // Debug.Log("First finger entered!");
                 }
-                else if(isPlayerLeft && !userTouchedLeft) { // If the player is on the left and the user touched the right side
-                    totalMovement.x = 3.5f;
-                    isPlayerLeft = false; // Player is now on the right
-                }
-
-                // Set the new position
-                transform.position = new Vector2(transform.position.x + totalMovement.x, transform.position.y + totalMovement.y);
-                // Debug.Log("First finger entered!");
+            }
+        }
+        else
+        {
+            t += Time.deltaTime / 0.1f;
+            transform.position = Vector2.Lerp(transform.position, newPosition, t);
+            if (transform.position.Equals(newPosition))
+            {
+                moving = false;
             }
         }
     }
