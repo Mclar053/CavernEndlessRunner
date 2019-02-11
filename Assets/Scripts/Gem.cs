9868e81b65ce308148433b2fd169d9f4dd014e32
Matthew Clark <matrclark@gmail.com>
2019-02-11 22:05:18 +0000

    Added Lava, Gems & Score
    Adding scoring mechanic and gems which add 10 points. Added lava to force the player to move.

---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    private int score;
+
+    private void Start()
+    {
+        score = 10;
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            coll.GetComponent<PlayerControls>().addScore(score);
+            Destroy(gameObject);
+        }
+    }
+}
